5e52e9f189d71474fee9cd7690a19d7be15b0ded
longnnt <longnnt99@gmail.com>
2024-11-06 20:24:26 +0700

    feature: add btc coin

--- constants/constants.go
+++ constants/constants.go
@@ -1,25 +1,30 @@
 package constants
 
 type Token struct {
-	Name string
+	Name    string
 	Address string
-	Symbol string
+	Symbol  string
 }
 
 var TOKEN_LIST = []Token{
 	{
-		Name: "BNB",
+		Name:    "Bitcoin",
+		Address: "0x71CCe0035d82c21Cf4b908bcd8F1117fFf0Fa623",
+		Symbol:  "BTC",
+	},
+	{
+		Name:    "BNB",
 		Address: "0xb8c77482e45f1f44de1745f52c74426c631bdd52",
-		Symbol: "BNB",
+		Symbol:  "BNB",
 	},
 	{
-		Name: "SHIBA",
+		Name:    "SHIBA",
 		Address: "0x2859e4544C4bB03966803b044A93563Bd2D0DD4D",
-		Symbol: "SHIB",
+		Symbol:  "SHIB",
 	},
 	{
-		Name: "USDT",
+		Name:    "USDT",
 		Address: "0xdac17f958d2ee523a2206206994597c13d831ec7",
-		Symbol: "USDC",
+		Symbol:  "USDC",
 	},
 }
